363163fecbbf0001d564e05311b0354e2f04489a
caelaward <146817394+caelaward@users.noreply.github.com>
2024-01-08 16:34:37 +0200

    added json data and displayed information on about page

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    about:[]
   },
   getters: {
   },
   mutations: {
+    setAbout(state,data){
+      state.about= data
+    }
   },
   actions: {
+    fetchData(context){
+     axios.get( "http://localhost:3000/about")
+     .then((a)=>{
+      console.log(a.data[0]);
+      context.commit("setAbout", a.data[0] )
+     })
+    } 
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{$store.state.about.description}}</p>
+    <p>{{$store.state.about.hobbies}}</p>
+    
   </div>
 </template>
+
+<script>
+export default {
+    computed:{
+        fetchData(){
+            this.$store.dispatch('fetchData')
+        }
+    },
+    mounted(){
+        this.fetchData
+    }
+}
+</script>

2f73dd6839baa8d80556e2d56cd4ebb819ce534d
caelaward <146817394+caelaward@users.noreply.github.com>
2024-01-09 16:15:49 +0200

    added data to testimonial page and projects page

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,11 +1,47 @@
 <template>
   <h1>Project Page</h1>
+  <!-- <div v-for="project in $store.state.projects" :key="project.project">
+    <div class="card " style="width: 18rem;">
+  <img :src="$store.state.project.image" class="card-img-top" alt="...">
+  <div class="card-body ">
+    <h5 class="card-title">{{project.project}}</h5>
+    <p class="card-text">{{project.description}}</p>
+    <a :href="project.github" class="btn btn-primary">github</a>
+     <a :href="project.vercel" class="btn btn-primary">vercel</a>
+  </div>
+</div>
+  
+
+  </div> -->
+  <div class="container">
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+ <div v-for="project in $store.state.projects" :key="project" class="col">
+    <div class="card">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <h5 class="card-title">{{project.project}}</h5>
+    <p class="card-text">{{project.description}}</p>
+     <a :href="project.github" class="btn btn-primary me-4">github</a>
+     <a :href="project.vercel" class="btn btn-primary ">vercel</a>
+     </div>
+    </div>
+  </div>
+</div>
+  </div>
   
 </template>
 
 <script>
 export default {
-
+   computed:{
+        fetchDataProjects(){
+            this.$store.dispatch('fetchDataProjects')
+        }
+    },
+    mounted(){
+        this.fetchDataProjects
+    }
 }
 </script>
 
--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,10 +1,36 @@
 <template>
   <h1>Testimonials Page</h1>
+  <div class="container">
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+ <div v-for="testimonial in $store.state.testimonials" :key="testimonial" class="col">
+    <div class="card">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
+      <div class="card-body">
+       <h5 class="card-title">{{testimonial.name}}</h5>
+      <p class="card-text">{{testimonial.description}}</p>
+
+      
+     
+     </div>
+    </div>
+  </div>
+</div>
+  </div>
+  
+  
 </template>
 
 <script>
 export default {
-
+   computed:{
+        fetchDataTestimonials(){
+            this.$store.dispatch('fetchDataTestimonials')
+        }
+    },
+    mounted(){
+        this.fetchDataTestimonials
+    }
 }
 </script>
 

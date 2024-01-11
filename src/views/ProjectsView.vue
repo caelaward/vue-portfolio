c7b03467c691b1ffb3a6853be481f84b38534642
caelaward <146817394+caelaward@users.noreply.github.com>
2024-01-11 11:38:34 +0200

    added changes to my resume page and json file

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -18,7 +18,7 @@
   <div class="row row-cols-1 row-cols-md-3 g-4">
  <div v-for="project in $store.state.projects" :key="project" class="col">
     <div class="card">
-      <img :src="project.image" class="card-img-top w-50" alt="...">
+      <img :src="project.image" class="card-img-top " alt="...">
       <div class="card-body">
         <h5 class="card-title">{{project.project}}</h5>
     <p class="card-text">{{project.description}}</p>

5b9cb0ab7dee54168629dd28fe444ea25884a9e0
caelaward <callaward32@gmail.com>
2024-04-05 13:31:36 +0200

    changed the toggle bar color

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -22,7 +22,7 @@
     </div> -->
     <nav class="navbar navbar-expand-lg p-3">
       <div class="container-fluid ">
-        <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
